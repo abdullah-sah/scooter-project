1deac7c55e906c6a514d05f9740b249fc4fb35fd
Abdullah Sahraoui <abdullahsahraoui@Abdullahs-MacBook-Air.local>
2022-10-22 15:31:22 +0100

    Code & tests for User class.

--- tests/User.test.js
+++ tests/User.test.js
@@ -1,9 +1,20 @@
 const User = require('../src/User')
-
+const testUser = new User('bob', 'letmein', 9);
 // User tests here
+describe('testing properties for the User class', () => { 
+	// test username
+	test('should have a username passed in as an argument', () => {
+		expect(typeof testUser.username).toBe('string');
+	});
 
-// test username
+	// test password
+	test('should have a password passed in as an argument', () => {
+		expect(typeof testUser.password).toBe('string');
+	})
 
-// test password
+	// test age
+	test('should have an age passed in as an argument', () => {
+		expect(typeof testUser.age).toBe('number');
+	})
 
-// test age
+ })
